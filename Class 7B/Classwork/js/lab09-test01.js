609ba2e193b2828a02e1396afdaf0cc13337b43d
Egrosberg <ecgrosberg@gmail.com>
2024-03-07 20:30:38 -0500

    classwork 7b

--- Class 7B/Classwork/js/lab09-test01.js
+++ Class 7B/Classwork/js/lab09-test01.js
@@ -1,2 +1,20 @@
 /* add code after this comment */
 
+// console.log(document.getElementById("thumb-list"));
+// // console.log(`MANY: ${document.getElementsByClassName{"thumb"}}`);
+// console.log(document.getElementsByClassName("thumbs"));
+
+document.getElementById('thumb-list').style.border = "solid 1px #CCCCCC";
+// console.log(document.getElementById('thumb-list'));
+// document.getElementById('thumb-list').style.display = 'none';
+
+document.querySelector('#msg').value=document.querySelector('.box fieldset p').textContent;
+
+const images=document.querySelectorAll('#thumb-list img');
+for (let img of images){
+    img.style.boxShadow = "6px 5px 20px 1px rgba(0,0,0,0.22)";
+    // console.log(img.style);
+};
+
+const d=document.querySelector('#msg');
+d.innerHTML = '<h2>HELLO WORLD<h2>';
