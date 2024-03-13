27b78b5abf7b93d65cabfbfde455299caed9f39b
Egrosberg <ecgrosberg@gmail.com>
2024-03-12 20:15:13 -0400

    Homework 7

--- Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
+++ Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
@@ -4,3 +4,12 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+   for (let item of cart){
+      outputCartRow(item, calculateTotal(item.quantity, item.product.price))
+   }
+
+   document.write('<tr class="totals"><td colspan="4">Subtotal</td><td>$'+subtotal()+'</td></tr>');
+   document.write('<tr class="totals"><td colspan="4">Tax</td><td>$'+calculateTax(tax_rate)+'</td></tr>');
+   document.write('<tr class="totals"><td colspan="4">Shipping</td><td>$'+calculateShipping(shipping_threshold)+'</td></tr>');
+   document.write('<tr class="totals"><td colspan="4" class="focus">Grand Total</td><td class="focus">$'+calculateGrandTotal(tax_rate, shipping_threshold)+'</td></tr>');
--- Class 7A/Homework/project1-pg413-14/js/functions.js
+++ Class 7A/Homework/project1-pg413-14/js/functions.js
@@ -1,10 +1,48 @@
 /* define your functions here */
+// Esther Chaya Grosberg
+// 3/10/24
+
+function calculateTotal(quantity, price){
+    return (quantity*price)
+}
+
+function outputCartRow(item, total){
+    document.write('<tr><td><img src=images/'+item.product.filename+'></td><td>'+item.product.title+'</td><td>'+item.quantity+'</td><td>$'+item.product.price.toFixed(2)+'</td><td>$'+total.toFixed(2)+'</td></tr>');
+}
 
 
+function subtotal(){
+    let amount = 0;
+    for (let item of cart){
+        amount=amount+calculateTotal(item.quantity, item.product.price);
+    }
+    return amount.toFixed(2);
+}
 
+function calculateTax(tax_rate){
+    let taxAmount=0;
+    for (let item of cart){
+        taxAmount=taxAmount+tax_rate*calculateTotal(item.quantity,item.product.price);
+    }
+    return taxAmount.toFixed(2);
+}
 
+function calculateShipping(shipping_threshold){
+    let total=0;
+    let shipping=0;
+    for (let item of cart){
+        total=total+calculateTotal(item.quantity, item.product.price);
+   }
+   if (total<shipping_threshold){
+        shipping=40;
+   }
+   return shipping.toFixed(2);
+}
 
+function calculateGrandTotal(tax_rate, shipping_threshhold){
+    var grandTotal=0;
+    grandTotal=parseInt(subtotal())+parseInt(calculateTax(tax_rate))+parseInt(calculateShipping(shipping_threshhold));
+    return grandTotal.toFixed(2);
 
+}
 
-
-        
